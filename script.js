ba60bb1b9a2b19d60168cd8cdb2cc218a021f6e4
DiyoDuende <elgrandiyo@gmail.com>
2026-05-05 16:40:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -236,12 +236,6 @@ aplicarTraduccion();
   }
 }
 setInterval(actualizarUIInterpretes, 15000);
-function t(key, vars = {}) {
-  let texto = textos[idiomaActual]?.[key] || key;
-
-  Object.keys(vars).forEach(k => {
-    texto = texto.replace(`{${k}}`, vars[k]);
-  });
 
   return texto;
 }
